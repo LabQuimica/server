441b32cb557051c8961fb9ab27ce2e512ceb0f5c
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-11-23 12:22:19 -0600

    login and register

--- querys/auth.js
+++ querys/auth.js
@@ -1,11 +1,46 @@
 import pool from '../database.js'
+import bycrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-export async function getUser(id){
-    const [rows] = await pool.query("SELECT * FROM users WHERE id_user = ?", [id])
+export async function postRegister(email, password, name){
+  // Verificar si el usuario ya existe
+  const [existingUser] = await  pool.query("SELECT email FROM users WHERE email =  ?", [email]);
+  if (existingUser.length > 0) {
+      throw new Error('User already exists');
+  }
+
+  // Insertar el nuevo usuario en la base de datos
+  const hashedPassword = await bycrypt.hash(password, 10);
+  const [result] = await pool.query(
+      "INSERT INTO users (email, password, name) VALUES (?, ?, ?)",
+      [email, hashedPassword, name]
+  );
+  const user = await getUserById(result.insertId);
+  return { user };
+}
+
+export async function postLogin(email, password){
+    // Verificar si el usuario ya existe
+    const [existingUser] = await pool.query("SELECT * FROM users WHERE email = ?", [email]);
+    if (existingUser.length === 0) {
+        throw new Error('User does not exist');
+    }
+
+    const isMatch = await bycrypt.compare(password, existingUser[0].password);
+    if (!isMatch) {
+        throw new Error('Contraseña incorrecta');
+    }
+
+    const token = jwt.sign({ id: existingUser[0].id_user }, process.env.JWT_SECRET, {
+        expiresIn: '3h'
+    });
+
+    return { token };
+  }
+  
+
+export async function getUserById(id){
+    const [rows] = await pool.query("SELECT id_user,email, name,date, rol FROM users WHERE id_user =  ?", [id])
     return rows[0]
 }
 
-export async function getUsers(){
-    const [rows] = await pool.query("SELECT * FROM users")
-    return rows
-}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,47 @@
 import express from 'express';
-import { getUser, getUsers } from '../querys/auth.js';
-
+import { getUserById, postRegister, postLogin} from '../querys/auth.js';
 const authRouter = express.Router();
 
+authRouter.post('/register', async (req, res) => {
+    const { email, password, name } = req.body;
+    console.log(email, password, name);
+    if (!email || !password || !name) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    try {
+        const newUser = await postRegister(email, password, name);
+        res.status(201).json({ message: 'User registered successfully', user: newUser });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+authRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    if (!email || !password ) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    try {
+        const token = await postLogin(email, password);
+        console.log(token);
+        res.status(201).json({ message: 'Login successfully', token: token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
 authRouter.get('/:id', async (req, res) => {
-    const user = await getUser(req.params.id);
+    const user = await getUserById(req.params.id);
     if (!user) {
         return res.status(404).json({ message: 'Usuario no encontrado :(' });
     }
     res.status(200).json(user);
 })
 
-authRouter.get('/', async (req, res) => {
-    const user = await getUsers();
-    if (!user) {
-        return res.status(404).json({ message: 'Usuario no encontrado :(' });
-    }
-    res.status(200).json(user);
-})
 
 export default authRouter;
