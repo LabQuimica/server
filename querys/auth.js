25c9f72f3fbaf5d17ae8aa2bce21dcfbe68766dd
DiegoCastr00 <diego.castro.elvira@gmail.com>
2025-04-11 01:05:11 -0600

    Add 'img' column to users table with default value and update user retrieval logic

--- querys/auth.js
+++ querys/auth.js
@@ -1,52 +1,67 @@
-import pool from '../database.js'
-import bycrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import pool from "../database.js";
+import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export async function postRegister(email, password, name, rol = 'alumno') {
+export async function postRegister(email, password, name, rol = "alumno") {
   // Verificar si el usuario ya existe
-  const [existingUser] = await  pool.query("SELECT email FROM users WHERE email =  ?", [email]);
+  const [existingUser] = await pool.query(
+    "SELECT email FROM users WHERE email =  ?",
+    [email]
+  );
   if (existingUser.length > 0) {
-      throw new Error('User already exists');
+    throw new Error("User already exists");
   }
 
   // Insertar el nuevo usuario en la base de datos
   const hashedPassword = await bycrypt.hash(password, 10);
   const [result] = await pool.query(
-      "INSERT INTO users (email, password, name, rol) VALUES (?, ?, ?, ?)",
-      [email, hashedPassword, name, rol]
+    "INSERT INTO users (email, password, name, rol) VALUES (?, ?, ?, ?)",
+    [email, hashedPassword, name, rol]
   );
   const user = await getUserById(result.insertId);
   return { user };
 }
 
 export async function postLogin(email, password) {
-    // Verificar si el usuario ya existe
-    const [existingUser] = await pool.query("SELECT * FROM users WHERE email = ?", [email]);
-    if (existingUser.length === 0) {
-        throw new Error('User does not exist :(');
+  // Verificar si el usuario ya existe
+  const [existingUser] = await pool.query(
+    "SELECT * FROM users WHERE email = ?",
+    [email]
+  );
+  if (existingUser.length === 0) {
+    throw new Error("User does not exist :(");
+  }
+
+  const isMatch = await bycrypt.compare(password, existingUser[0].password);
+  if (!isMatch) {
+    throw new Error("Contraseña incorrecta");
+  }
+
+  const token = jwt.sign(
+    { id: existingUser[0].id_user },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "3h",
     }
+  );
 
-    const isMatch = await bycrypt.compare(password, existingUser[0].password);
-    if (!isMatch) {
-        throw new Error('Contraseña incorrecta');
-    }
+  const user = {
+    id_user: existingUser[0].id_user,
+    name: existingUser[0].name,
+    email: existingUser[0].email,
+    date: existingUser[0].date,
+    rol: existingUser[0].rol,
+    active: existingUser[0].active,
+    codigo: existingUser[0].codigo,
+    img: existingUser[0].img,
+  };
 
-    const token = jwt.sign({ id: existingUser[0].id_user }, process.env.JWT_SECRET, {
-        expiresIn: '3h'
-    });
-
-    const user = {
-        id_user: existingUser[0].id_user,
-        name: existingUser[0].name,
-        email: existingUser[0].email,
-        date: existingUser[0].date,
-        rol: existingUser[0].rol,
-        active: existingUser[0].active
-    };
-
-    return { token, user };
+  return { token, user };
 }
-export async function getUserById(id){
-    const [rows] = await pool.query("SELECT id_user,email, name,date, rol FROM users WHERE id_user =  ?", [id])
-    return rows[0]
+export async function getUserById(id) {
+  const [rows] = await pool.query(
+    "SELECT id_user,email, name,date, rol FROM users WHERE id_user =  ?",
+    [id]
+  );
+  return rows[0];
 }
